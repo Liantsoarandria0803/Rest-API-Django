f15a133e84f0dd1d8983165a79910fad24e2d34e
Liantsoa <liantsoarandrianasimbolarivelo@gmail.com>
2024-11-13 13:53:22 +0300

    Post,Get,Put,Delete implementation

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet
+from .views import book_detail,create_book
 
-router = DefaultRouter()
-router.register(r'books', BookViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('book/<int:pk>/',book_detail,name='book' ),
+    path('create',create_book,name='create_book'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from django.shortcuts import render,get_object_or_404
+from rest_framework.decorators import api_view 
+from rest_framework import status
+from rest_framework.response import Response
 from .models import Book
 from .serializers import BookSerializer
 
-class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-# Create your views here.
+@api_view(['POST'])
+def create_book(request):
+    serializer=BookSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET','PUT','DELETE'])
+def book_detail(request, pk):
+    try:
+        book = Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = BookSerializer(book)  # Utiliser l'instance 'book'
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = BookSerializer(book, data=request.data)  # Corriger 'Book' en 'book'
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        book.delete()  # Utiliser 'book.delete()' au lieu de 'Book.delete()'
+        return Response(status=status.HTTP_204_NO_CONTENT)
